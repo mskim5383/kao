bd771fadaec25d1ec148f8e77c7dfa3cb0e1690a
Minsoo Kim <mskim5383@gmail.com>
2016-10-08 04:42:45 +0900

    Add ConcertContent model

--- project/apps/public_web/admin.py
+++ project/apps/public_web/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from apps.public_web.models import WebContent
+from apps.public_web.models import WebContent, ConcertContent
 
 # Register your models here.
 admin.site.register(WebContent)
+admin.site.register(ConcertContent)
